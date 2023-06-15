ca85af5ff4dceaf50c77cc6b309cd364549d1b22
mindeleven <mindeleven@posteo.de>
2023-06-15 12:39:16 +0200

    Refactoring the example program using tuples

--- rust-programming-language-bk/ch05a-structs-example/src/main.rs
+++ rust-programming-language-bk/ch05a-structs-example/src/main.rs
@@ -1,10 +1,20 @@
+// example program calculating the area of a rectangel
 fn main() {
-    let height1 = 30;
-    let width1 = 50;
+    // (1) using single variables
+    // let height1 = 30;
+    // let width1 = 50;
+    // (2) refactoring using tuples
+    let rect1 = (30, 50);
 
-    println!("The area of the rectangle is {} square pixels.", area(width1, height1));
+    // println!("The area of the rectangle is {} square pixels.", area(width1, height1));
+    println!("The area of the rectangle is {} square pixels.", area(rect1));
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
+
