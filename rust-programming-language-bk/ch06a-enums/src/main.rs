b5e4a913780ab9cdbf27fd5f1cbb60f1f334ac54
mindeleven <mindeleven@posteo.de>
2023-06-16 13:29:37 +0200

    Defining function that takes any IpAddrKind enum

--- rust-programming-language-bk/ch06a-enums/src/main.rs
+++ rust-programming-language-bk/ch06a-enums/src/main.rs
@@ -8,6 +8,12 @@
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
-    println!("Enum holds IP addresses of type four({:?}) and six({:#?})", four, six);
+    // println!("Enum holds IP addresses of type four({:?}) and six({:#?})", four, six);
+    route(four);
+    route(six);
 }
 
+// defining function that takes any IpAddrKind enum
+fn route(ip_kind: IpAddrKind) {
+    println!("Ip address of type {:?}", ip_kind)
+}
