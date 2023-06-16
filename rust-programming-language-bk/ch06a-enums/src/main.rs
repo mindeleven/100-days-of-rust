774df9bfebd5e2f072492f99317c6b406fc76603
mindeleven <mindeleven@posteo.de>
2023-06-16 13:27:27 +0200

    Defining basic enum

--- rust-programming-language-bk/ch06a-enums/src/main.rs
+++ rust-programming-language-bk/ch06a-enums/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+// defining enum to hold IP addresses
+ #[derive(Debug)]
+ enum IpAddrKind {
+    V4,
+    V6
 }
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    println!("Enum holds IP addresses of type four({:?}) and six({:#?})", four, six);
+}
+
