bba1c268d7e007840e1ec0927f4af12cee8851ee
mindeleven <mindeleven@posteo.de>
2023-06-16 13:37:33 +0200

    Added struct to store IP addresses

--- rust-programming-language-bk/ch06a-enums/src/main.rs
+++ rust-programming-language-bk/ch06a-enums/src/main.rs
@@ -2,7 +2,14 @@
  #[derive(Debug)]
  enum IpAddrKind {
     V4,
-    V6
+    V6,
+}
+
+// struct to store IP addresses
+ #[derive(Debug)]
+ struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
 }
 
 fn main() {
@@ -11,6 +18,20 @@ fn main() {
     // println!("Enum holds IP addresses of type four({:?}) and six({:#?})", four, six);
     route(four);
     route(six);
+
+    // instantiating struct to store IP adresses
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    println!("IP address home: {:#?}", home);
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+    println!("loopback: {:#?}", loopback);
+    
 }
 
 // defining function that takes any IpAddrKind enum
