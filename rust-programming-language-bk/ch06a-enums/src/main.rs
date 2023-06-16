5433d30ee0732e3785491f7fd6b925271bb152d7
mindeleven <mindeleven@posteo.de>
2023-06-16 14:04:36 +0200

    Refactored code to store data in enum itself

--- rust-programming-language-bk/ch06a-enums/src/main.rs
+++ rust-programming-language-bk/ch06a-enums/src/main.rs
@@ -1,16 +1,26 @@
 // defining enum to hold IP addresses
+// instead of using a struct to store IP adresses
+// the enum can directly be used to store data
  #[derive(Debug)]
  enum IpAddrKind {
     V4,
     V6,
 }
 
-// struct to store IP addresses
+// an enum can directly be used to store data
+// each type can have different types of associated data
  #[derive(Debug)]
- struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+ enum IpAddrKindV2 {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
+
+// struct to store IP addresses
+//  #[derive(Debug)]
+//  struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
 
 fn main() {
     let four = IpAddrKind::V4;
@@ -20,18 +30,21 @@ fn main() {
     route(six);
 
     // instantiating struct to store IP adresses
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    // let home = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1"),
+    // };
+    let home = IpAddrKindV2::V4(127, 0, 0, 1);
     println!("IP address home: {:#?}", home);
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    // let loopback = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1"),
+    // };
+    let loopback: IpAddrKindV2 = IpAddrKindV2::V6(String::from("::1"));
     println!("loopback: {:#?}", loopback);
-    
+
+
 }
 
 // defining function that takes any IpAddrKind enum
