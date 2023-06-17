db9862431c1150bd89e71b6104668e7ec27f5b28
mindeleven <mindeleven@posteo.de>
2023-06-17 15:44:58 +0200

    Basic notation of enums

--- rust-programming-language-bk/ch06b-option-enums/src/main.rs
+++ rust-programming-language-bk/ch06b-option-enums/src/main.rs
@@ -1,3 +1,18 @@
+/// Option<T> enum definition in the standard library
+/// enum Option<T>
+/// { 
+///     None,
+///     Some(T)
+/// }
+/// <T> is the syntax for a generic type
+/// it means the Some variant can hold any piece of data
+
 fn main() {
-    println!("Hello, world!");
+    // Some and None can be used directly without the Option:: prefix
+    let _some_number = Some(5);
+    let _some_char = Some('e');
+    // for None the overall Option<T> type needs to be annotated
+    let _absent_number: Option<i32> = None;
+
+    println!("Hello Option<T> enums!");
 }
