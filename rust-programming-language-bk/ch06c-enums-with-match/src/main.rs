0ee21620f09dc46df5c0fd7a21f82e15e82d0e26
mindeleven <mindeleven@posteo.de>
2023-06-19 10:06:19 +0200

    Match arm modified to bind value

--- rust-programming-language-bk/ch06c-enums-with-match/src/main.rs
+++ rust-programming-language-bk/ch06c-enums-with-match/src/main.rs
@@ -1,23 +1,50 @@
 // match control flow construct
+// allows to compare values against a series of patterns
+// and execute code depending on the match
 #[allow(dead_code)]
 #[derive(Debug)]
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter
+    // from 1999 to 2008 quarters were minted with state logo
+    // so let's add the state as data
+    Quarter(UsState)
+}
+
+// patterns that bind to values
+// match arms can bind to the part of the values that match the pattern
+#[allow(dead_code)]
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // just to name a few
 }
 
 fn main() {
-    let picked_coin = Coin::Nickel;
-    println!("The value of this coin is {}.", value_in_cents(picked_coin));
+    println!("The value of this coin is {:?}.", value_in_cents(Coin::Nickel));
+    println!("The value of this coin is {:?}.", value_in_cents(Coin::Penny));
+    println!("The value of this coin is {:?}.", value_in_cents(Coin::Quarter(UsState::Alaska)));
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        // the match arms
+        // each has two parts: a pattern and some code
+        Coin::Penny => {
+            // match arm with multiple lines of code
+            println!("Lucky penny!");
+            1
+        },
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        // adjusted pattern according to altered enum variant
+        Coin::Quarter(state) => {
+            // when coin matches 
+            // state variable will be bound to the value of the quarter's state
+            println!("State quarter from {:?}", state);
+            25
+        },
     }
 }
