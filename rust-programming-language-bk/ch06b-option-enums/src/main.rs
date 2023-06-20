4142acda5eb421e50b2d05f3cf6fed5e122cbb93
mindeleven <mindeleven@posteo.de>
2023-06-20 14:47:40 +0200

    Examples for match statements

--- rust-programming-language-bk/ch06b-option-enums/src/main.rs
+++ rust-programming-language-bk/ch06b-option-enums/src/main.rs
@@ -15,4 +15,49 @@ fn main() {
     let _absent_number: Option<i32> = None;
 
     println!("Hello Option<T> enums!");
+
+    // catch all patterns
+    let dice_roll = 9;
+
+    // act according to dice roll in a game
+    match dice_roll {
+        // actions according to dice roll
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        // last arm covers all other possible values
+        _ => reroll(),
+    }
+
+    // match with _ placeholder (catch all)
+    match dice_roll {
+        // actions according to dice roll
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        // last arm covers all other possible values
+        // _ matches any value but does not bind to any value
+        other => move_player(other),
+    }
+
+    // match with _ placeholder and empty tuple (unity value)
+    match dice_roll {
+        // actions according to dice roll
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        // last arm covers all other possible values
+        // _ matches any value but does not bind to any value
+        // empty tuple indicates that nothing should happen
+        _ => (),
+    }
 }
+
+// dummy function to add a hat in a game
+fn add_fancy_hat() {}
+
+// dummy function to remove a hat in a game
+fn remove_fancy_hat() {}
+
+// dummy function to remove a player in a game
+fn move_player(_spaces: u8) {}
+
+// dummy function to reroll the dice in a game
+fn reroll() {}
