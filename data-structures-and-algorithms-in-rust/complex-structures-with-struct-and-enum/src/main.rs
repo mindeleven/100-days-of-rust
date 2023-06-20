b794086a4d8f55bb50a53d8bcdecd3befe801a91
mindeleven <mindeleven@posteo.de>
2023-06-20 10:37:51 +0200

    Added enum example

--- data-structures-and-algorithms-in-rust/complex-structures-with-struct-and-enum/src/main.rs
+++ data-structures-and-algorithms-in-rust/complex-structures-with-struct-and-enum/src/main.rs
@@ -5,6 +5,14 @@ struct Person {
     children: i32,
 }
 
+#[derive(Debug)]
+#[allow(dead_code)]
+enum Color {
+    Red,
+    Green, 
+    Blue
+}
+
 impl Person {
     /// to use Person after call to print self needs to be passed as reference
     /// otherwise ownership is transfered to method
@@ -26,4 +34,12 @@ fn main() {
     println!("Hello, people, from {}!", p.print());
     // print struct to get debug information
     println!("#[derive(Debug)] allows to get debug information: {:?}", p);
+
+    let c = Color::Red;
+
+    match c {
+        Color::Red => println!("It's red"),
+        Color::Green => println!("It's green"),
+        Color::Blue => println!("It's blue"),
+    }
 }
