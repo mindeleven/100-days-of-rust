afcb20b81f5f01e01a53e2eb600b88097d7c1a0b
mindeleven <mindeleven@posteo.de>
2023-06-20 10:10:33 +0200

    Basic struct with impl block

--- data-structures-and-algorithms-in-rust/complex-structures-with-struct-and-enum/src/main.rs
+++ data-structures-and-algorithms-in-rust/complex-structures-with-struct-and-enum/src/main.rs
@@ -1,3 +1,24 @@
+struct Person {
+    name: String,
+    age: i32,
+    children: i32,
+}
+
+impl Person {
+    pub fn print(self) -> String {
+        format!(
+            "name = {}, age = {}, has {} children", 
+            self.name, self.age, self.children
+        )
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let p = Person {
+        name: "Matt".to_string(),
+        age: 35, 
+        children: 4,
+    };
+
+    println!("Hello, people, from {}!", p.print());
 }
