eaf894b8176412f36fe8142f1661cc2b17d50789
mindeleven <mindeleven@posteo.de>
2023-06-23 15:01:38 +0200

    Eliminating duplicating code by introducing a generic type parameter

--- rust-programming-language-bk/ch10a-generic-types/src/main.rs
+++ rust-programming-language-bk/ch10a-generic-types/src/main.rs
@@ -11,6 +11,16 @@ fn main() {
     let char_list = vec!['a', 'c', 'r', 'y', 'z', 'q', 'o'];
     let result = largest_char(&char_list);
     println!("Largest number in list is {}", result);
+
+    // once again with the generic function
+    let number_list: Vec<i32> = vec![34, 87, 50, 100, 98, 2, 175];
+    let result = largest(&number_list);
+    println!("Largest number in list is {}", result);
+
+    let char_list = vec!['a', 'c', 'r', 'y', 'z', 'q', 'o'];
+    let result = largest(&char_list);
+    println!("Largest number in list is {}", result);
+
 }
 
 // function that finds the largest value in a slice of i32s
@@ -35,6 +45,17 @@ fn largest_char(list: &[char]) -> &char {
     largest
 }
 
-/// when defining a function 
-/// generics are placed in the signature of the function
-fn largest_gen() {}
+/// eliminating duplicating code by introducing a generic type parameter
+/// when defining a function generics are placed in the signature of the function
+/// not all types are sortable and can be applied to T
+/// compiler suggests fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
