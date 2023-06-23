9dfc505b98ff39e996e6b4f30de29d3e36325ad1
mindeleven <mindeleven@posteo.de>
2023-06-23 14:18:07 +0200

    2nd example to show how to eliminate duplication for functionality with different data types

---
+++ rust-programming-language-bk/ch10a-generic-types/src/main.rs
@@ -0,0 +1,8 @@
+/// chapter 10, generic types, traits, and lifetimes
+/// generics allow us to replace specific types 
+/// with a placeholder the represents multiple types
+/// 2nd example to show how to eliminate duplication
+/// for functionality with different data types
+fn main() {
+    println!("Hello, world!");
+}
