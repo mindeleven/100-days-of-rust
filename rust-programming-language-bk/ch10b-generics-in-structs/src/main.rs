2232870309c13879a478a1f67716c0ad45b7202b
mindeleven <mindeleven@posteo.de>
2023-06-24 13:29:35 +0200

    Defining a struct with different generic types

--- rust-programming-language-bk/ch10b-generics-in-structs/src/main.rs
+++ rust-programming-language-bk/ch10b-generics-in-structs/src/main.rs
@@ -2,19 +2,32 @@
 /// generics allow us to replace specific types 
 /// with a placeholder the represents multiple types
 /// 3rd example, generics in struct definitions
-/// /// Generic types 3rd example, generics in struct definitions
+/// 
+/// using generics in a struct definition
+/// defininition with only one generic type
+/// struct is generic over some type T
 #[derive(Debug)]
 #[allow(dead_code)]
 struct Point<T> {
     x: T,
     y: T,
 }
+
+// defining a struct with different generic types
+#[derive(Debug)]
+#[allow(dead_code)]
+struct PointV2<T, U> {
+    x: T,
+    y: U,
+}
 fn main() {
     let integer = Point { x: 5, y: 10};
     let float = Point { x: 1.0, y: 4.0 };
+    let int_float = PointV2 { x: 5, y: 3.22};
 
     println!("Point struct of type integer: {:?}", integer);
     println!("Point struct of type float: {:?}", float);
+    println!("Point struct of type integer and float: {:?}", int_float);
 }
 
 
