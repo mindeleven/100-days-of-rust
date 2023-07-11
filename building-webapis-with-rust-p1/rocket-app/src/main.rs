060bda1d5d8769309d35dccf11cd13a7513e1935
mindeleven <mindeleven@posteo.de>
2023-07-11 13:34:18 +0200

    Modifying endpoint to return json

--- building-webapis-with-rust-p1/rocket-app/src/main.rs
+++ building-webapis-with-rust-p1/rocket-app/src/main.rs
@@ -1,11 +1,15 @@
 /// instructing the compiler that we're gonna make use of rocket macros
 #[macro_use] extern crate rocket;
+// importing/making use of json
+use rocket::serde::json::{Value, json};
 
 /// defining endpoint with attribute
 /// incoming get request to "/"
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!\n"
+// returning Value from serde::json
+fn hello() -> Value {
+    // making use of json makro
+    json!("Hello, world!")
 }
 
 /// main function will build rocket framework
