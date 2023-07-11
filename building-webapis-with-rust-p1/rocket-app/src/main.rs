c4089cad68d0bff9dcddda44dbc8476b8388434e
mindeleven <mindeleven@posteo.de>
2023-07-11 12:33:17 +0200

    Making use of the rocket macro and defining an endpoint

--- building-webapis-with-rust-p1/rocket-app/src/main.rs
+++ building-webapis-with-rust-p1/rocket-app/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+/// instructing the compiler that we're gonna make use of rocket macros
+#[macro_use] extern crate rocket;
+
+/// defining endpoint with attribute
+/// incoming get request to "/"
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!\n"
+}
+
+/// main function will build rocket framework
+/// after launch it will start listening to the requests
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
+        .mount("/", routes![hello])
+        .launch()
+        .await;
 }
