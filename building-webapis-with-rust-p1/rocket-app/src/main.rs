099309b71245ef20b4f92534c4b2625290f4c531
mindeleven <mindeleven@posteo.de>
2023-07-11 14:40:35 +0200

    Added CRUD endpoints

--- building-webapis-with-rust-p1/rocket-app/src/main.rs
+++ building-webapis-with-rust-p1/rocket-app/src/main.rs
@@ -1,16 +1,51 @@
 /// instructing the compiler that we're gonna make use of rocket macros
 #[macro_use] extern crate rocket;
 // importing/making use of json
-use rocket::serde::json::{Value, json};
+use rocket::{serde::json::{Value, json}, response::status};
 
+/// CRUD endpoints
+/// GET list existing
+/// GET show single
+/// POST create new
+/// PUT update existing
+/// DELETE delete existing
+/// 
+/// CRUD endpoints for fictional rustaceans database
+#[get("/rustaceans")]
+fn get_rustaceans() -> Value {
+    json!([
+        {"id": 3, "name": "John Doe", "email": "john@doe.com"},
+        {"id": 4, "name": "John Again Doe", "email": "john.again@doe.com"}
+    ])
+}
+#[get("/rustaceans/<id>")]
+fn view_rustaceans(id: i32) -> Value {
+    json!({"id": 3, "name": "John Doe", "email": "john@doe.com"})
+}
+#[post("/rustaceans", format="json")]
+fn create_rustaceans() -> Value {
+    json!({"id": 3, "name": "John Doe", "email": "john@doe.com"})
+}
+#[put("/rustaceans/<id>", format="json")]
+fn update_rustaceans(id: i32) -> Value {
+    json!({"id": id, "name": "John Doe", "email": "john@doe.com"})
+}
+#[delete("/rustaceans/<id>")]
+fn delete_rustaceans(id: i32) -> status::NoContent {
+    status::NoContent
+}
+
+/// 
 /// defining endpoint with attribute
 /// incoming get request to "/"
 #[get("/")]
-// returning Value from serde::json
+/// returning Value from serde::json
 fn hello() -> Value {
     // making use of json makro
     json!("Hello, world!")
 }
+
+/// 
 
 /// main function will build rocket framework
 /// after launch it will start listening to the requests
