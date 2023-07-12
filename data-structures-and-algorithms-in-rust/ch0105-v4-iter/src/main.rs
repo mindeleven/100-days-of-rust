a8559d8787ad6cfcf83b8fdcf254c9fb9073105e
mindeleven <mindeleven@posteo.de>
2023-07-12 12:20:33 +0200

    Loop, while and for examples

--- data-structures-and-algorithms-in-rust/ch0105-v4-iter/src/main.rs
+++ data-structures-and-algorithms-in-rust/ch0105-v4-iter/src/main.rs
@@ -1,3 +1,31 @@
+/// looping mechanisms in iterators
+/// 
+
 fn main() {
-    println!("Hello, world!");
+    let mut n = 0;
+
+    // loop with break condition
+    loop {
+        n += 1;
+        if n > 10 {
+            break;
+        }
+        println!("Hello, {} -> world!", n);
+    }
+    println!("All done with loop!");
+
+    // doing the same with while
+    // no break condition needed
+    while n < 20 {
+        n += 1;
+        println!("Hello, {} -> world!", n);
+    }
+    println!("All done with while!");
+
+    // for loop with range object
+    for i in 0..10 {
+        println!("for loop, {} -> world", i);
+    }
+    println!("All done with for!");
+
 }
