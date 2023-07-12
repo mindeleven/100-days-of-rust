4ad84fc90c87b6b0d363525aca28d06a5e67e0e8
mindeleven <mindeleven@posteo.de>
2023-07-12 13:31:34 +0200

    Implementing Iterator for a struct

--- data-structures-and-algorithms-in-rust/ch0105-v4-iter/src/main.rs
+++ data-structures-and-algorithms-in-rust/ch0105-v4-iter/src/main.rs
@@ -1,5 +1,26 @@
+#![allow(dead_code)]
 /// looping mechanisms in iterators
 /// 
+pub struct Stepper {
+    curr: i32,
+    step: i32,
+    max: i32,
+}
+
+/// giving functionality of a trait from the standard library to the Stepper
+/// if you implement Iterator for an object you can use it in a for loop
+impl Iterator for Stepper {
+    type Item = i32;
+    fn next(&mut self) -> Option<i32> {
+        if self.curr >= self.max {
+            return None;
+        }
+        let res = self.curr;
+        self.curr += self.step;
+        Some(res)
+
+    }
+}
 
 fn main() {
     let mut n = 0;
@@ -27,5 +48,33 @@ fn main() {
         println!("for loop, {} -> world", i);
     }
     println!("All done with for!");
+    
 
+    // iterrating through the stepper
+    let mut st = Stepper {
+        curr: 2,
+        step: 3,
+        max: 15,
+    };
+    // iterrating through the stepper with loop
+    loop {
+        match st.next() {
+            Some(v) => println!("stepper loop {}", v),
+            None => break,
+        }
+    }
+    // iterrating through the stepper with while
+    let mut st2 = Stepper {
+        curr: 3,
+        step: 4,
+        max: 15,
+    };
+    while let Some(n) = st2.next() {
+        println!("stepper while, {}", n);
+    }
+    // iterrating through the stepper with for
+    let it = Stepper { curr:5, step:10, max:50 };
+    for i in it {
+        println!("for stepper, {} -> world", i);
+    }
 }
